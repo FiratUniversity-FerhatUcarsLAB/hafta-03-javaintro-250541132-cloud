c8cbc87bf74268c1b11a8428a79432c6b6fe000c
250541132-cloud <250541132@firat.edu.tr>
2025-10-23 12:56:06 +0300

    Update JavaDeseni.java

---
+++ Task-1-HosgeldinMesajlari/JavaDeseni.java
@@ -0,0 +1,9 @@
+
+public class JavaDeseni{
+    public static void main(String[] args)  {
+        System.out.println("   J  A  V  A");
+        System.out.println("  JAA  V V A");
+        System.out.println(" J A  VVV AA ");
+        System.out.println("JJAAVVAA");
+    }
+}
